7f9416c339eb2e7cf8d4cff3c4ae2d800fe8637a
Jifeng Zhang <zjfroot@gmail.com>
2012-04-05 12:10:55 +0200

    Simple version of paypal button

--- main.py
+++ main.py
@@ -16,41 +16,14 @@ sys.path.append('modules')
 from add import AddNoteHandler
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        user = users.get_current_user()
-        if user:
-            if user.email() == "zjfroot@gmail.com":
-                #get word list
-                query = db.GqlQuery("SELECT * FROM Note ORDER BY created DESC LIMIT 30")
-                notes = query.fetch(20)
-                notes_today = []
-                notes_yesterday = []
-                notes_older = []
-                for note in notes:
-                    diff = (date.today() - note.created.date())
-                    if diff == timedelta (days = 0):
-                        notes_today.append(note)
-                    elif diff == timedelta (days = 1):
-                        notes_yesterday.append(note)
-                    else:
-                        notes_older.append(note)
-                
+    def get(self):                
                 author = 'Jifeng Zhang'
                 action = 'programming'
                 template_values = {
-                    'author':author,
-                    'action':action,
-                    'notes_today':notes_today,
-                    'notes_yesterday':notes_yesterday,
-                    'notes_older':notes_older,
                 }
-                path = os.path.join(os.path.dirname(__file__),'index.html')
+                #path = os.path.join(os.path.dirname(__file__),'index.html')
+                path = os.path.join(os.path.dirname(__file__),'index_simple.html')
                 self.response.out.write(template.render(path,template_values))
-            else:
-                self.redirect(users.create_login_url(self.request.uri))
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
-    
         
 class OolongBasicHandler(webapp.RequestHandler):
     def get(self, id):
